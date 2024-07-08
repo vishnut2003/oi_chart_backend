14aef5d2a7e8941b462de7c05ecf7c356728e30b
Vishnu <iam.vishnu.bhaskar@gmail.com>
2024-07-08 19:46:06 +0530

    OI data working

--- helpers/breezeHelpers.js
+++ helpers/breezeHelpers.js
@@ -1,49 +1,70 @@
 const fsPromise = require('fs/promises')
 const path = require('path')
-const BreezeConnect = require('uatbreezeconnect').BreezeConnect
+const BreezeConnect = require('breezeconnect').BreezeConnect
 
 module.exports = {
     getApiSessionKey: async () => {
         const sessionKey = await fsPromise.readFile(path.join(__dirname, '..', 'routes', 'credentials', 'access_token.txt'), { encoding: 'utf8' })
         return sessionKey
     },
-    getNiftyData: (sessionKey, expiry) => {
+    getNiftyData: (sessionKey) => {
         const apiKey = process.env.API_KEY
         const secretKey = process.env.SECRET_KEY
 
-        const breeze = new BreezeConnect({
-            "appKey": apiKey
-        })
-        breeze.generateSession({
-            "secretKey": secretKey,
-            "sessionToken": sessionKey
-        })
+        const breeze = new BreezeConnect({ "appKey": apiKey })
+
+        breeze.generateSession(secretKey, sessionKey)
             .then((res) => {
-                const date = new Date()
-                const year = date.getFullYear()
-                const month = date.getMonth()
-                const day = date.getDate()
+                breeze.getOptionChainQuotes(
+                    {
+                        stockCode: "NIFTY",
+                        exchangeCode: "NFO",
+                        productType: "options",
+                        expiryDate: "2024-07-11T06:00:00.000Z",
+                        right: "call",
+                    }
+                )
+                    .then((res) => {
+                        const strike_price = []
+                        const callOiData = []
 
-                const fromDate = new Date(year, 6, day, 9, 15, 0).toISOString()
-                const toDate = new Date(year, 6, day, 17, 30, 0).toISOString()
+                        const fullData = res.Success
+                        for (let i = 0; i < fullData.length; i++) {
+                            strike_price.push(fullData[i].strike_price)
+                        }
 
-                breeze.getHistoricalData({
-                    interval: '5minute',
-                    fromDate: fromDate,
-                    toDate: toDate,
-                    stockCode: 'NIFTY',
-                    exchangeCode: "NFO",
-                    productType: "options",
-                    expiryDate: expiry,
-                    right: "call",
-                    strikePrice: '9000'
-                })
-                .then((res) => {
-                    console.log(res)
-                })
-                .catch((err) => {
-                    console.log(err)
-                })
+                        for (let i = 0; i < strike_price.length; i++) {
+                            breeze.getHistoricalDatav2(
+                                {
+                                    interval: "30minute",       //'1second', '1minute', '5minute', '30minute','1day'
+                                    fromDate: "2024-07-03T07:00:00.000Z",
+                                    toDate: "2024-07-04T07:00:00.000Z",
+                                    stockCode: "NIFTY",
+                                    exchangeCode: "NFO",      // 'NSE','BSE','NFO','NDX,'MCX'
+                                    productType: "options",   // "futures","options",'cash'
+                                    expiryDate: "2024-07-11T07:00:00.000Z",
+                                    right: "call",           // "call","put", "others" 
+                                    strikePrice: strike_price[i]
+                                }
+                            )
+                                .then((res) => {
+                                    if (!callOiData[i]) {
+                                        if (res.Success !== undefined) {
+                                            if (res.Success.length != 0) {
+                                                callOiData.push(res.Success[i])
+                                                console.log(res.Success)
+                                            }
+                                        }
+                                    }
+                                    else callOiData[i].strike_price += res.Success[i].strike_price
+                                })
+
+                        }
+                        const finale = []
+
+                        console.log(callOiData)
+
+                    })
             })
             .catch((err) => {
                 console.log(err)
